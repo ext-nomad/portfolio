9dd8f7e71d23b9048cc86f6f24e73248126318d7
ext-nomad <i.dwanderer@gmail.com>
2021-01-26 13:19:21 +0300

    add/ custom carousel

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,4 +12,53 @@ require("stylesheets/application.scss");
 
 $(document).on("turbolinks:load", function () {
   $('[data-toggle="tooltip"]').tooltip();
+
+  var carousel = document.querySelector(".carousel");
+  var cells = carousel.querySelectorAll(".carousel_cell");
+  var cellCount = carousel.dataset.cells;
+  var selectedIndex = 0;
+  var radius, theta;
+  // console.log( cellWidth, cellHeight );
+
+  function rotateCarousel() {
+    var angle = theta * selectedIndex * -1;
+    carousel.style.transform =
+      "translateZ(" + -radius + "px) " + "rotateY" + "(" + angle + "deg)";
+  }
+
+  var prevButton = document.querySelector(".previous-button");
+  prevButton.addEventListener("click", function () {
+    selectedIndex--;
+    rotateCarousel();
+  });
+
+  var nextButton = document.querySelector(".next-button");
+  nextButton.addEventListener("click", function () {
+    selectedIndex++;
+    rotateCarousel();
+  });
+
+  function changeCarousel() {
+    theta = 360 / cellCount;
+    var cellSize = carousel.offsetWidth;
+    radius = Math.round(cellSize / 2 / Math.tan(Math.PI / cellCount));
+    for (var i = 0; i < cells.length; i++) {
+      var cell = cells[i];
+      if (i < cellCount) {
+        // visible cell
+        cell.style.opacity = 1;
+        var cellAngle = theta * i;
+        cell.style.transform =
+          "rotateY" + "(" + cellAngle + "deg) translateZ(" + radius + "px)";
+      } else {
+        // hidden cell
+        cell.style.opacity = 0;
+        cell.style.transform = "none";
+      }
+    }
+
+    rotateCarousel();
+  }
+
+  changeCarousel();
 });
